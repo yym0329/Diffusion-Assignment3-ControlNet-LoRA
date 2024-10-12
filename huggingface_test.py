f57483ed867887befd0ba4b2115e191d6289f328
youngminRyou <yym3055@gmail.com>
2024-10-12 07:09:33 +0000

    add test generation images

---
+++ huggingface_test.py
@@ -0,0 +1,14 @@
+import torch
+from diffusers import StableDiffusionPipeline
+
+model_id = "CompVis/stable-diffusion-v1-4"
+device = "cuda"
+
+
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to(device)
+
+prompt = "forest green circle with an antique brown background"
+image = pipe(prompt).images[0]  
+    
+image.save("test_4.png")
